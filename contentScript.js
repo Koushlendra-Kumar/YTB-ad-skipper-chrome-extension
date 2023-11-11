47eb6169e4c6b359b644e973499196fca66dd66e
Web Dev Koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2023-11-11 09:45:08 +0530

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -4,6 +4,12 @@ function clickSkipButton() {
         skipButton[0].click();
         console.log('clicked');
     }
+    const oldSkipButton = document.getElementsByClassName('ytp-ad-skip-button');
+    
+    if(oldSkipButton.length > 0) {
+        oldSkipButton[0].click();
+        console.log('clicked');
+    }
 }
 
 clickSkipButton();
