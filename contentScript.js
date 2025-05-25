a39f391263a3e4e9703dce38f16d926ee1404e83
Koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2025-05-25 10:06:11 +0530

    Remove skip button click as it was not working

--- contentScript.js
+++ contentScript.js
@@ -1,51 +1,52 @@
-function clickSkipButton() {
-  [
-    "ytp-ad-skip-button-modern",
-    "ytp-ad-skip-button",
-    "ytp-skip-ad-button",
-  ].forEach((cls) => {
-    const skipButton = document.getElementsByClassName(cls);
-    if (skipButton.length > 0) {
-      skipButton[0].click();
-      console.log("clicked", cls);
-    }
-  });
-}
+// function clickSkipButton() {
+//   [
+//     "ytp-ad-skip-button-modern",
+//     "ytp-ad-skip-button",
+//     "ytp-skip-ad-button",
+//   ].forEach((cls) => {
+//     const skipButton = document.getElementsByClassName(cls);
+//     if (skipButton.length > 0) {
+//       skipButton[0].click();
+//       console.log("clicked", cls);
+//     }
+//   });
+// }
 
-function hideFeedAdd() {
-  const adds = document.querySelectorAll("ytd-ad-slot-renderer");
-  adds.forEach((add) => {
-    console.log("hiding ad");
-    add.style.display = "none";
-  });
+function hideFeedAds() {
+  const ads = document.querySelectorAll("ytd-ad-slot-renderer");
+  if (ads.length > 0) {
+    ads.forEach((add) => {
+      add.style.display = "none";
+    });
+  }
 }
 
 function hideYoutubeShorts() {
   const shorts = document.querySelectorAll("ytd-reel-shelf-renderer");
   const homePageShorts = document.querySelectorAll("ytd-rich-section-renderer");
-  homePageShorts.forEach((short) => {
-    console.log("hiding home shorts");
-    short.style.display = "none";
-  });
-  shorts.forEach((short) => {
-    console.log("hiding shorts");
-    short.style.display = "none";
-  });
+  if (homePageShorts.length > 0) {
+    homePageShorts.forEach((short) => {
+      short.style.display = "none";
+    });
+  }
+
+  if (shorts.length > 0) {
+    shorts.forEach((short) => {
+      short.style.display = "none";
+    });
+  }
 }
 
 function hideYoutubeShortsEntryButton() {
-  const shortsEntry = document.querySelectorAll(
-    "#endpoint[title='Shorts']"
-  );
-  console.log(shortsEntry, shortsEntry.length);
+  const shortsEntry = document.querySelectorAll("#endpoint[title='Shorts']");
   if (shortsEntry.length > 0) {
     shortsEntry.forEach((entry) => (entry.style.display = "none"));
   }
 }
 
 setInterval(() => {
-  hideFeedAdd();
+  hideFeedAds();
   hideYoutubeShortsEntryButton();
   hideYoutubeShorts();
-  clickSkipButton();
+  // clickSkipButton();
 }, 1000);
